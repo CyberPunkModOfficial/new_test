4f5c546f6a426a54657fc02fb192a39a28eff3d8
CyberPunkModOfficial <gaudianocharles2@gmail.com>
2026-09-11 13:01:13 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,7 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_bf7.mk
 
 COMMON_LUNCH_CHOICES := \
+    twrp_bf7-user \
+    twrp_bf7-userdebug \
     twrp_bf7-eng
